2e89cacdf29ed1f815f864e53a9fc55eb593c786
Hyun <dacapo629@naver.com>
2026-03-01 04:24:08 +0900

    feature: 엔티티,리포지 및 구현체, 컨트롤러, 서비스 추가

--- src/main/java/com/git/prac/store/infrastructure/repository/StoreRepositoryImpl.java
+++ src/main/java/com/git/prac/store/infrastructure/repository/StoreRepositoryImpl.java
@@ -1,4 +1,24 @@
 package com.git.prac.store.infrastructure.repository;
 
-public class StoreRepositoryImpl {
+import com.git.prac.store.domain.entity.StoreEntity;
+import com.git.prac.store.domain.repository.StoreRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class StoreRepositoryImpl implements StoreRepository {
+    private final StoreJpaRepository storeJpaRepository;
+
+
+    @Override
+    public StoreEntity save(StoreEntity storeEntity) {
+        return storeJpaRepository.save(storeEntity);
+    }
+
+    @Override
+    public Optional<StoreEntity> findById(Long id) {
+        return storeJpaRepository.findById(id);
+    }
 }
